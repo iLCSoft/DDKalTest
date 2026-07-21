78ad6b8429309587055463c422b16ff47ae778ce
Juan Miguel Carceller <22276694+jmcarcell@users.noreply.github.com>
2026-07-21 08:39:34 +0200

    Add missing const

--- src/DDPlanarMeasLayer.cc
+++ src/DDPlanarMeasLayer.cc
@@ -62,7 +62,7 @@ DDPlanarMeasLayer::DDPlanarMeasLayer(dd4hep::rec::ISurface* surf, Double_t   Bz,
 
     // get global/local origin
     const dd4hep::rec::Vector3D& o = surf->origin() ;
-    dd4hep::rec::VolSurface vs = ((dd4hep::rec::Surface*)surf)->volSurface();
+    const dd4hep::rec::VolSurface vs = ((dd4hep::rec::Surface*)surf)->volSurface();
     const dd4hep::rec::Vector3D& oL = vs.origin() ;
 
     // dd4hep::rec::Vector3D oR( o[0] , o[1] , 0 ) ; // radial direction of origin in global coordinates
